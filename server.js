6b0c23178e7a63d649399c245e95f6652f39b4d7
Kent Hua <khua@redhat.com>
2017-08-17 14:44:47 -0700

    fixed for tsb

--- server.js
+++ server.js
@@ -12,28 +12,21 @@ app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURL = "",
     mongoURLLabel = "";
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
+mongoDatabase = 'sampledb',
+mongoPassword = process.env['password']
+mongoUser = process.env['username'];
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+var uri = process.env['uri'];
+var mongodb_prefix = uri.substr(0,10);
+var mongodb_end = uri.substring(11,uri.count);
+mongoURL = mongodb_prefix + mongoUser + ':' + mongoPassword + '@' + mongodb_end;
+mongoURLLabel = mongoURL;
 
-  }
-}
+console.log('MongoURL:' + mongoURL);
+
 var db = null,
     dbDetails = new Object();
 
