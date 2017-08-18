debec19d9aa0d3017b84adaf71ce6d92f698bbc2
Kent Hua <khua@redhat.com>
2017-08-18 06:27:00 -0700

    fixed substring method

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoPassword = process.env['password']
 mongoUser = process.env['username'];
 
 var uri = process.env['uri'];
-var mongodb_prefix = uri.substr(0,10);
+var mongodb_prefix = uri.substring(0,10);
 var mongodb_end = uri.substring(10,uri.count);
 mongoURL = mongodb_prefix + mongoUser + ':' + mongoPassword + '@' + mongodb_end;
 mongoURLLabel = mongoURL;
